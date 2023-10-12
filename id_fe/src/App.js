febf88bf52f09fe7c64f744058bce50aeaaed647
ngdangha <ndha.it.cva@gmail.com>
2023-10-13 00:17:11 +0700

    Update id_fe

---
+++ id_fe/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import Header from "components/Header.js";
+import Footer from "components/SimpleFooter.js";
+import logo from './logo.svg';
+import './style.css';
+
+
+function App() {
+  const [user, setUser] = useState(null);
+  const [disease, setDisease] = useState(null);
+  const [mapData, setMapData] = useState(null);
+  const [blogTag, setBlogTag] = useState([]);
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;

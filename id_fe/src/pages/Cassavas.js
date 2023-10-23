db5074e4861f103f8b8c88627ee37226ea3ebb34
Nguyen Do Duong <duongnd1820hp@gmail.com>
2023-10-23 11:05:25 +0700

    update form
    update color and form

--- id_fe/src/pages/Cassavas.js
+++ id_fe/src/pages/Cassavas.js
@@ -240,66 +240,144 @@ export default () => {
 
   return (
     <AnimationRevealPage>
-      <div className="flex flex-col w-[450px] text-lg">
-        <Heading>
-                  <HighlightedText1>Personal information</HighlightedText1>
-        </Heading>
-      </div>
+          <div style={{ marginTop: "20px", marginLeft: "40px" }}>
+              <div style={{ position: "relative" }}>
+                  <div
+                      style={{
+                          position: "relative",
+                          background: "#0D99FF",
+                          padding: "10px",
+                          borderRadius: "10px",
+                          width: "fit-content",
+                      }}
+                  >
+                      <p style={{ color: "white", fontWeight: "bold", display: "inline" }}>
+                          Personal information
+                      </p>
+                  </div>
+                  <div style={{ marginTop: "10px", display: "flex" }}>
+                      <div style={{ marginTop: "0px", marginLeft: "20px" }}>
+                          <p>Name:</p>
+                          <p>Gender:</p>
+                          <p>Date of birth:</p>
+                          <p>Province/city:</p>
+                          <p>District:</p>
+                          <p>ID number:</p>
+                          <p>Phone number:</p>
+                          <p>Email:</p>
+                      </div>
+                      <div style={{ marginTop: "0px", marginLeft: "60px" }}>
+                          <p>Lo Anh Duc</p>
+                          <p>Male</p>
+                          <p>01/02/1995</p>
+                          <p>Hanoi</p>
+                          <p>19021243</p>
+                          <p>034101007845</p>
+                          <p>0328919074</p>
+                          <p>ducla@hanoihospital.com</p>
+                      </div>
+                  </div>
 
-      <div>
-        <div className="text-lg">
-          <p><strong>Name:</strong> Lo Anh Duc</p>
-          <p><strong>Gender:</strong> Male</p>
-          <p><strong>Date of birth:</strong> xx/xx/xxxx</p>
-          <p><strong>Province/City:</strong> xxxx</p>
-          <p><strong>District:</strong> xxxx</p>
-          <p><strong>ID number:</strong> xxxxxxxx</p>
-          <p><strong>Phone number:</strong> xxxxxxxx</p>
-          <p><strong>Email:</strong> xxxx@gmail.com</p>
-        </div>
-      </div>
+                  <div
+                      style={{
+                          marginTop: "20px",
+                          position: "relative",
+                          background: "#0D99FF",
+                          padding: "10px",
+                          borderRadius: "10px",
+                          width: "fit-content",
+                      }}
+                  >
+                      <p
+                          style={{ color: "white", fontWeight: "bold", display: "inline" }}
+                      >
+                          Patient management
+                      </p>
+                  </div>
+                  <div style={{ marginTop: "10px", display: "flex" }}>
+                      <div style={{ marginTop: "0px", marginLeft: "20px" }}>
+                          <p>Medical history:</p>
+                          <p>Drug using history:</p>
+                          <p>Family medical history:</p>
+                      </div>
+                      <div style={{ marginTop: "0px", marginLeft: "60px" }}>
+                          <p></p>
+                          <p></p>
+                          <p></p>
+                      </div>
+                  </div>
+                  <div
+                      style={{
+                          marginTop: "20px",
+                          position: "relative",
+                          background: "#0D99FF",
+                          padding: "10px",
+                          borderRadius: "10px",
+                          width: "fit-content",
+                      }}
+                  >
+                      <p
+                          style={{ color: "white", fontWeight: "bold", display: "inline" }}
+                      >
+                          Hospital medical history
+                      </p>
+                  </div>
+                  <div style={{ marginTop: "10px", display: "flex" }}>
+                      <div style={{ marginTop: "0px", marginLeft: "20px" }}>
+                          <p><strong>No.01</strong></p>
+                          <p>Date:</p>
+                          <p>Hospital:</p>
+                          <p>Doctors:</p>
+                          <p>Symptoms:</p>
+                          <p>Tests:</p>
+                          <p>Diagnose:</p>
+                          <p>Drug:</p>
+                          <p>Advice:</p>
+                          <p>Follow-up appointment:</p>
+                      </div>
+                      <div style={{ marginTop: "0px", marginLeft: "60px" }}>
+                          <p>&nbsp;</p>
+                          <p>18/09/2021 </p>
+                          <p>Thanh Hoa hospital </p>
+                          <p>Ms Nguyen Van A</p>
+                          <p>xxx</p>
+                          <p>xxx, xxx, xxx</p>
+                          <p>xxx </p>
+                          <p>Paracetamol xxx</p>
+                          <p>xxx</p>
+                          <p>xxx</p>
+                      </div>
+                  </div>
+                  <div style={{ marginTop: "10px", display: "flex" }}>
+                      <div style={{ marginTop: "0px", marginLeft: "20px" }}>
+                          <p><strong>No.02</strong></p>
+                          <p>Date:</p>
+                          <p>Hospital:</p>
+                          <p>Doctors:</p>
+                          <p>Symptoms:</p>
+                          <p>Tests:</p>
+                          <p>Diagnose:</p>
+                          <p>Drug:</p>
+                          <p>Advice:</p>
+                          <p>Follow-up appointment:</p>
+                      </div>
+                      <div style={{ marginTop: "0px", marginLeft: "60px" }}>
+                          <p>&nbsp;</p>
+                          <p>21/10/2022 </p>
+                          <p>Thanh Hoa hospital </p>
+                          <p>Mr Tran Thi B</p>
+                          <p>xxx </p>
+                          <p>xxx, xxx, xxx</p>
+                          <p>xxx </p>
+                          <p>Paracetamol xxx</p>
+                          <p>xxx</p>
+                          <p>xxx</p>
+                      </div>
+                  </div>
+              </div>
 
-      <div className="flex flex-col w-[450px] text-lg">
-        <Heading>
-                  <HighlightedText1>Self-Declaration</HighlightedText1>
-        </Heading>
-      </div>
-      <div className="text-lg">
-          <p><strong>Medical history:</strong> </p>
-          <p><strong>Drug using history:</strong> </p>
-          <p><strong>Family medical history:</strong> </p>
-      </div>
-      <div className="flex flex-col w-[450px] text-lg">
-        <Heading>
-                  <HighlightedText1>Hospital medical history</HighlightedText1>
-        </Heading>
-      </div>
-      <div className="text-lg">
-          <p><strong>No.01</strong> </p>
-          <p><strong>Date:</strong> 18/09/2021 </p>
-          <p><strong>Hospital:</strong> Thanh Hoa hospital </p>
-          <p><strong>Doctors:</strong> Ms Nguyen Van A</p>
-          <p><strong>Symptoms:</strong> xxx </p>
-          <p><strong>Tests:</strong> xxx, xxx, xxx</p>
-          <p><strong>Diagnose:</strong> xxx </p>
-          <p><strong>Drug:</strong> Paracetamol xxx</p>
-          <p><strong>Advice:</strong> xxx</p>
-          <p><strong>Follow-up appointment:</strong> xxx</p>
-          <p><strong>No.02</strong> </p>
-          <p><strong>Date:</strong> 21/10/2022 </p>
-          <p><strong>Hospital:</strong> Thanh Hoa hospital </p>
-          <p><strong>Doctors:</strong> Mr Tran Thi B</p>
-          <p><strong>Symptoms:</strong> xxx </p>
-          <p><strong>Tests:</strong> xxx, xxx, xxx</p>
-          <p><strong>Diagnose:</strong> xxx </p>
-          <p><strong>Drug:</strong> Paracetamol xxx</p>
-          <p><strong>Advice:</strong> xxx</p>
-          <p><strong>Follow-up appointment:</strong> xxx</p>        
-      </div>
-
-                      
-                  
-   
+              
+          </div>  
     </AnimationRevealPage>
   );
 };

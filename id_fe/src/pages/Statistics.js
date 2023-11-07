5ab6b4949e9b78fb271262f6e0114e6d55ab2e17
Nguyen Do Duong <duongnd1820hp@gmail.com>
2023-11-07 21:03:13 +0700

    update
    update

--- id_fe/src/pages/Statistics.js
+++ id_fe/src/pages/Statistics.js
@@ -1,6 +1,6 @@
 ﻿import { React, useState, } from "react";
 import LineChart from "components/LineChart";
-import { DiseaseData } from "Data";
+import { DiseaseData, PredictedDiseaseData1m, PredictedDiseaseData3m, PredictedDiseaseData6m, PredictedDiseaseData2m, PredictedDiseaseData12m} from "Data";
 import { useTranslation } from "react-i18next";
 
 export default () => {
@@ -13,11 +13,33 @@ export default () => {
             datasets: [{
                 label: "Number of Cases",
                 data: DiseaseData.map((data) => data.flu),
+                hidden: false,
             },
             {
-                label: "Number of Cases Prediction",
-                data: [null, null, null, null, null, null, null, 3, 3, 3, 1, 0],
-            }],
+                label: "1 Month Prediction",
+                data: PredictedDiseaseData1m.map((data) => data.flu),
+                hidden: true,
+            },
+            {
+                label: "2 Months Prediction",
+                data: PredictedDiseaseData2m.map((data) => data.flu),
+                hidden: true,
+            },
+            {
+                label: "3 Months Prediction",
+                data: PredictedDiseaseData3m.map((data) => data.flu),
+                hidden: true,
+            },
+            {
+                label: "6 Months Prediction",
+                data: PredictedDiseaseData6m.map((data) => data.flu),
+                hidden: true,
+            },
+            {
+                label: "12 Months Prediction",
+                data: PredictedDiseaseData12m.map((data) => data.flu),
+                hidden: true,
+            },],
         })
     const [covid19Data, setCovid19Data] = useState(
         {
@@ -25,11 +47,33 @@ export default () => {
             datasets: [{
                 label: "Number of Cases",
                 data: DiseaseData.map((data) => data.covid19),
+                hidden: false,
             },
             {
-                label: "Number of Cases Prediction",
-                data: [null, null, null, null, null, null, null, 3, 1, 3, 1, 1],
-            }],
+                label: "1 Month Prediction",
+                data: PredictedDiseaseData1m.map((data) => data.covid19),
+                hidden: false,
+            },
+            {
+                label: "2 Months Prediction",
+                data: PredictedDiseaseData2m.map((data) => data.covid19),
+                hidden: false,
+            },
+            {
+                label: "3 Months Prediction",
+                data: PredictedDiseaseData3m.map((data) => data.covid19),
+                hidden: false,
+            },
+            {
+                label: "6 Months Prediction",
+                data: PredictedDiseaseData6m.map((data) => data.covid19),
+                hidden: false,
+            },
+            {
+                label: "12 Months Prediction",
+                data: PredictedDiseaseData12m.map((data) => data.covid19),
+                hidden: false,
+            },],
         })
     const [pneumoniaData, setPneumoniaData] = useState(
         {
@@ -37,11 +81,33 @@ export default () => {
             datasets: [{
                 label: "Number of Cases",
                 data: DiseaseData.map((data) => data.pneumonia),
+                hidden: false,
             },
             {
-                label: "Number of Cases Prediction",
-                data: [null, null, null, null, null, null, null, 10, 9, 6, 8, 9],
-            }],
+                label: "1 Month Prediction",
+                data: PredictedDiseaseData1m.map((data) => data.pneumonia),
+                hidden: false,
+            },
+            {
+                label: "2 Months Prediction",
+                data: PredictedDiseaseData2m.map((data) => data.pneumonia),
+                hidden: false,
+            },
+            {
+                label: "3 Months Prediction",
+                data: PredictedDiseaseData3m.map((data) => data.pneumonia),
+                hidden: false,
+            },
+            {
+                label: "6 Months Prediction",
+                data: PredictedDiseaseData6m.map((data) => data.pneumonia),
+                hidden: false,
+            },
+            {
+                label: "12 Months Prediction",
+                data: PredictedDiseaseData12m.map((data) => data.pneumonia),
+                hidden: false,
+            },],
         })
     const [hepatitisData, setHepatitisData] = useState(
         {
@@ -49,11 +115,33 @@ export default () => {
             datasets: [{
                 label: "Number of Cases",
                 data: DiseaseData.map((data) => data.hepatitis),
+                hidden: false,
             },
             {
-                label: "Number of Cases Prediction",
-                data: [null, null, null, null, null, null, null, 1, 1, 2, 2, 1],
-            }],
+                label: "1 Month Prediction",
+                data: PredictedDiseaseData1m.map((data) => data.hepatitis),
+                hidden: false,
+            },
+            {
+                label: "2 Months Prediction",
+                data: PredictedDiseaseData2m.map((data) => data.hepatitis),
+                hidden: false,
+            },
+            {
+                label: "3 Months Prediction",
+                data: PredictedDiseaseData3m.map((data) => data.hepatitis),
+                hidden: false,
+            },
+            {
+                label: "6 Months Prediction",
+                data: PredictedDiseaseData6m.map((data) => data.hepatitis),
+                hidden: false,
+            },
+            {
+                label: "12 Months Prediction",
+                data: PredictedDiseaseData12m.map((data) => data.hepatitis),
+                hidden: false,
+            },],
         })
     const [chickenpoxData, setChickenpoxData] = useState(
         {
@@ -61,11 +149,33 @@ export default () => {
             datasets: [{
                 label: "Number of Cases",
                 data: DiseaseData.map((data) => data.chickenpox),
+                hidden: false,
             },
             {
-                label: "Number of Cases Prediction",
-                data: [null, null, null, null, null, null, null, 1, 1, 3, 4, 2],
-            }],
+                label: "1 Month Prediction",
+                data: PredictedDiseaseData1m.map((data) => data.chickenpox),
+                hidden: false,
+            },
+            {
+                label: "2 Months Prediction",
+                data: PredictedDiseaseData2m.map((data) => data.chickenpox),
+                hidden: false,
+            },
+            {
+                label: "3 Months Prediction",
+                data: PredictedDiseaseData3m.map((data) => data.chickenpox),
+                hidden: false,
+            },
+            {
+                label: "6 Months Prediction",
+                data: PredictedDiseaseData6m.map((data) => data.chickenpox),
+                hidden: false,
+            },
+            {
+                label: "12 Months Prediction",
+                data: PredictedDiseaseData12m.map((data) => data.chickenpox),
+                hidden: false,
+            },],
         })
     const [dengueData, setDengueData] = useState(
         {
@@ -73,11 +183,34 @@ export default () => {
             datasets: [{
                 label: "Number of Cases",
                 data: DiseaseData.map((data) => data.dengue),
+                hidden: false,
             },
             {
-                label: "Number of Cases Prediction",
-                data: [null, null, null, null, null, null, null, 6, 3, 2, 3, 2],
-            }],
+                label: "1 Month Prediction",
+                data: PredictedDiseaseData1m.map((data) => data.dengue),
+                hidden: false,
+            },
+            {
+                label: "2 Months Prediction",
+                data: PredictedDiseaseData2m.map((data) => data.dengue),
+                hidden: false,
+            },
+            {
+                label: "3 Months Prediction",
+                data: PredictedDiseaseData3m.map((data) => data.dengue),
+                hidden: false,
+            },
+            {
+                label: "6 Months Prediction",
+                data: PredictedDiseaseData6m.map((data) => data.dengue),
+                hidden: false,
+            },
+            {
+                label: "12 Months Prediction",
+                data: PredictedDiseaseData12m.map((data) => data.dengue),
+                hidden: false,
+            },
+            ],
         })
     const [tuberculosisData, setTuberculosisData] = useState(
         {
@@ -85,37 +218,55 @@ export default () => {
             datasets: [{
                 label: "Number of Cases",
                 data: DiseaseData.map((data) => data.tuberculosis),
+                hidden: false,
             },
             {
-                label: "Number of Cases Prediction",
-                data: [null, null, null, null, null, null, null, 2, 1, 3, 4, 2],
-            }],
+               label: "1 Month Prediction",
+                data: PredictedDiseaseData1m.map((data) => data.tuberculosis),
+                hidden: false,
+            },
+            {
+               label: "2 Months Prediction",
+                data: PredictedDiseaseData2m.map((data) => data.tuberculosis),
+                hidden: false,
+            },
+            {
+               label: "3 Months Prediction",
+                data: PredictedDiseaseData3m.map((data) => data.tuberculosis),
+                hidden: false,
+            },
+            {
+               label: "6 Months Prediction",
+                data: PredictedDiseaseData6m.map((data) => data.tuberculosis),
+                hidden: false,
+            },
+            {
+               label: "12 Months Prediction",
+                data: PredictedDiseaseData12m.map((data) => data.tuberculosis),
+                hidden: false,
+            },],
 
         })
 
     return (
-
-        <div style={{ marginTop: "20px", display: "flex" }}>
-            <div style={{ marginTop: "0px", marginLeft: "120px" }}>
+        <div style={{ marginTop: "0px", display: "flex" }}>
+            <div style={{ flex: 1, height: "260px", marginTop: "15px", marginLeft: "140px" }}>
                 <LineChart chartData={fluData} />
-                <h1 style={{ marginTop: "8px", marginBottom: "22px", marginLeft: "80px", fontWeight: "bold" }}>{t('statistic.flu')}</h1>
+                <h1 style={{ marginTop: "10px", marginBottom: "30px", marginLeft: "180px", fontWeight: "bold" }}>Number of Flu Cases Chart</h1>
                 <LineChart chartData={covid19Data} />
-                <h1 style={{ marginTop: "8px", marginBottom: "22px", marginLeft: "75px", fontWeight: "bold" }}>{t('statistic.covid')}</h1>
-
-            </div>
-            <div style={{ marginTop: "0px", marginLeft: "160px" }}>
+                <h1 style={{ marginTop: "10px", marginBottom: "60px", marginLeft: "170px", fontWeight: "bold" }}>Number of Covid19 Cases Chart</h1>
                 <LineChart chartData={hepatitisData} />
-                <h1 style={{ marginTop: "8px", marginBottom: "22px", marginLeft: "78px", fontWeight: "bold" }}>{t('statistic.hepatitis')}</h1>
+                <h1 style={{ marginTop: "10px", marginBottom: "30px", marginLeft: "160px", fontWeight: "bold" }}>Number of Hepatitis Cases Chart</h1>
                 <LineChart chartData={chickenpoxData} />
-                <h1 style={{ marginTop: "8px", marginBottom: "22px", marginLeft: "76px", fontWeight: "bold" }}>{t('statistic.chickenpox')}</h1>
-                <LineChart chartData={pneumoniaData} />
-                <h1 style={{ marginTop: "8px", marginBottom: "22px", marginLeft: "72px", fontWeight: "bold" }}>{t('statistic.pneumonia')}</h1>
+                <h1 style={{ marginTop: "10px", marginBottom: "30px", marginLeft: "155px", fontWeight: "bold" }}>Number of Chickenpox Cases Chart</h1>
             </div>
-            <div style={{ marginTop: "0px", marginLeft: "160px" }}>
+            <div style={{ flex: 1, height: "260px", marginTop: "15px", marginLeft: "40px" }}>
+                <LineChart chartData={pneumoniaData} />
+                <h1 style={{ marginTop: "10px", marginBottom: "30px", marginLeft: "165px", fontWeight: "bold" }}>Number of Pneumonia Cases Chart</h1>
                 <LineChart chartData={dengueData} />
-                <h1 style={{ marginTop: "8px", marginBottom: "22px", marginLeft: "55px", fontWeight: "bold" }}>{t('statistic.dengue')}</h1>
+                <h1 style={{ marginTop: "10px", marginBottom: "60px", marginLeft: "170px", fontWeight: "bold" }}>Number of Dengue Cases Chart</h1>
                 <LineChart chartData={tuberculosisData} />
-                <h1 style={{ marginTop: "8px", marginBottom: "22px", marginLeft: "92px", fontWeight: "bold" }}>{t('statistic.tuberculosis')}</h1>
+                <h1 style={{ marginTop: "10px", marginBottom: "30px", marginLeft: "158px", fontWeight: "bold" }}>Number of Tuberculosis Cases Chart</h1>
             </div>
         </div>
     );
